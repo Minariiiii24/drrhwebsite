c61eae0866dcd43e55275f57e3377b4d85b26050
Darniel-Zidane-Garcia <69941482+Darniel-Zidane-Garcia@users.noreply.github.com>
2021-03-02 14:48:01 +0800

    Update main.js

--- Mamba/assets/js/main.js
+++ Mamba/assets/js/main.js
@@ -207,5 +207,22 @@
   $(window).on('load', function() {
     aos_init();
   });
+  
+    // Script ni Zaiboy=================================================
 
+  function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+  // END Script ni Zaiboy=================================================
+  
 })(jQuery);
